ed0e6d3f2a3bcfd7479e1860b6aa0d8e510b0469
Craig Rodrigues <PagingCraig@gmail.com>
2017-05-28 13:10:12 -0400

    406 complete

---
+++ 406-QueueReconstruction.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+const reconstructQueue = (people) => {
+  if (people.length === 0) return []
+
+  let results = []
+  // Stable sort people array
+  people.sort((a, b) => b[0] > a[0])
+
+  console.log(people)
+
+  // Iterate over array
+  for (let i = 0; i < people.length; i++) {
+    // If k index exists in array
+    let k = people[i][1]
+    if (results[k] !== undefined) {
+      // Remove from array and place into that index
+      results.splice(k, 0, people[i])
+    } else {
+      // Push to end of array
+      results.push(people[i])
+    }
+  }
+
+  return results
+}
+
+console.log(reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]])) // [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
